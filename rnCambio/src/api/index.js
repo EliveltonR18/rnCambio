2f323095199ba48cf7f4c892d19f3c2bf5a4b307
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2019-11-02 18:53:54 -0300

    [master] | app functional

--- rnCambio/src/api/index.js
+++ rnCambio/src/api/index.js
@@ -4,10 +4,4 @@ const api = axios.create({
     baseURL: 'https://api.exchangeratesapi.io/'
 });
 
-const fromBase = base => {
-    api.get(`latest?base=${base}`);
-}
-
-module.exports = {
-    fromBase,
-}
+export default api;
--- rnCambio/src/screens/App.js
+++ rnCambio/src/screens/App.js
@@ -1,11 +1,61 @@
 import React, {Fragment, Component} from 'react';
 import { SafeAreaView, StyleSheet, View, Text, StatusBar, TextInput, TouchableOpacity, 
-    ScrollView, KeyboardAvoidingView } from 'react-native';
+    ScrollView, KeyboardAvoidingView, Picker } from 'react-native';
 import * as _ from 'lodash';
+
+import api from '../api';
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: [],
+            coin1: 'EUR',
+            coin2: '',
+            coinsL: [],
+            result: '',
+        }
+    }
+
+    async componentDidMount() {
+        const coinsL = [];
+        const coins = await api.get('latest?base=EUR');
+        this.setState({ options: coins.data });
+        for (var chave in coins.data.rates) {
+            let moeda = {
+                moeda: chave,
+                valor: coins.data.rates[chave],
+            }
+            coinsL.push(moeda);
+        };
+        this.setState({ coinsL: coinsL })
+    }
+
+    async result(coin, coinX) {
+        const coinsL = [];
+        const coins = await api.get(`latest?base=${coin}`);
+        this.setState({ options: coins.data });
+        for (var chave in coins.data.rates) {
+            let moeda = {
+                moeda: chave,
+                valor: coins.data.rates[chave],
+            }
+            coinsL.push(moeda);
+        };
+
+        this.setState({ coinsL: coinsL });
+        
+        coinsL.map(coinL => {
+            if (coinL.moeda === coinX) {
+                this.setState({ result: coinL.valor });
+            }
+        })
+    }
+
     render() {
+        const { coinsL } = this.state;
+        
         return (
             <Fragment>
                 <StatusBar backgroundColor='#008b8b' />
@@ -18,7 +68,44 @@ class App extends Component {
                         >
                             <View style={styles.header}>
                                 <Text style={styles.title}>
-                                    Exchange
+                                    Quanto vale?
+                                </Text>
+                            </View>
+
+                            <View style={styles.coin1}>
+                                <Text style={styles.text}>Selecione as moedas: </Text>
+                                <Picker
+                                    selectedValue={this.state.coin1}
+                                    style={{height: 50, width: 300, backgroundColor: "rgba(0, 138, 138, 0.20)"}}
+                                    onValueChange={(item) => {
+                                            this.setState({coin1: item})
+                                        }
+                                    }
+                                    
+                                >
+                                    {coinsL.map(item => {
+                                        return(<Picker.Item label={item.moeda} value={item.moeda}/>);
+                                    })}
+                                </Picker>
+                            </View>
+                            <View style={styles.coin2}>
+                                <Picker
+                                    selectedValue={this.state.coin2}
+                                    style={{height: 50, width: 300, backgroundColor: "rgba(0, 138, 138, 0.35)"}}
+                                    onValueChange={(item) => {
+                                            this.setState({coin2: item});
+                                            this.result(this.state.coin1, item);
+                                        }
+                                    }
+                                >
+                                    {coinsL.map(item => {
+                                        return(<Picker.Item label={item.moeda} value={item.moeda} key={item.valor}/>);
+                                    })}
+                                </Picker>
+                            </View>
+                            <View style={styles.coin1}>
+                                <Text style={styles.text}>
+                                    {`1 ${this.state.coin1} vale ${this.state.result} ${this.state.coin2}`}
                                 </Text>
                             </View>
                         </KeyboardAvoidingView>
@@ -39,38 +126,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#008b8b',
         alignItems: "center"
     },
-    textInput: {
-        borderStyle: 'solid',
-        borderWidth:1,
-        borderRadius: 8,
-        fontSize:16,
-        color:'#008b8b',
-        padding:10,
+    coin1: {
+        paddingHorizontal: 40,
+        paddingVertical: 20
+    }, 
+    coin2: {
+        paddingHorizontal: 40,
     },
-    viewInput: {
-        paddingHorizontal: 20
-    },
-    gasText: {
-        paddingVertical: 20,
-        fontSize: 20
-    },
-    containerButton:{
-        paddingVertical:50,
-        alignItems: 'center'
-    },
-    button: {
-        backgroundColor:'#008b8b',
-        width:100,
-        height:40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 8,
-    },
-    textButton: {
-        textAlign:'center',
-        fontSize:16,
-        alignSelf: "center",
-        color: '#ffffff'
+    text:{
+        fontSize: 20,
+        paddingBottom: 30,
     }
 });
 
